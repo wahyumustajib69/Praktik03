f900163c10eede695a5580894a9f62e4ac830109
Wahyu <adjieb.wahyu@gmail.com>
2018-04-25 21:00:46 +0800

    Membuat Attribut

--- src/praktik03/Praktik03.java
+++ src/praktik03/Praktik03.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktik03;
 
 /**
@@ -10,12 +5,8 @@ package praktik03;
  * @author WAHYU'S PC
  */
 public class Praktik03 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
--- src/praktik03/Sepeda.java
+++ src/praktik03/Sepeda.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktik03;
 
 /**
@@ -10,5 +5,7 @@ package praktik03;
  * @author WAHYU'S PC
  */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
 }
