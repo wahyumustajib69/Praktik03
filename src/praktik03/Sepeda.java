4f8b1e1c8232b70fa3a867d7942231539565ceda
Wahyu <adjieb.wahyu@gmail.com>
2018-04-25 21:24:47 +0800

    buat Getter and Setter

--- src/praktik03/Sepeda.java
+++ src/praktik03/Sepeda.java
@@ -14,4 +14,29 @@ public class Sepeda {
         System.out.println("Warna    : "+warna);
         System.out.println("Harga    : "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
 }
