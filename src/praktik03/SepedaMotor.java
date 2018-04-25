b8e842f9468002d938b35af09e80314d331faf66
Wahyu <adjieb.wahyu@gmail.com>
2018-04-25 21:36:14 +0800

    Attribute dan Method

--- src/praktik03/SepedaMotor.java
+++ src/praktik03/SepedaMotor.java
@@ -1,4 +1,8 @@
 package praktik03;
-public class SepedaMotor {
+public class SepedaMotor extends Sepeda {
+    private String jenismotor;
     
+    void throttle(){
+        System.out.println("Ngeeeng....");
+    }
 }
