8fae19da7f72a90ce79816569ad6abdc76f81beb
Wahyu <adjieb.wahyu@gmail.com>
2018-04-25 21:27:16 +0800

    menggunakan methode setter

--- src/praktik03/SepedaAksi.java
+++ src/praktik03/SepedaAksi.java
@@ -2,9 +2,9 @@ package praktik03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk="Kupu-Kupu";
-        s.warna="Merah";
-        s.harga=1000;
+        s.setMerk("Kupu-Kupu");
+        s.setWarna("MERAH");
+        s.setHarga(1000);
                 
         s.cetakinfo();
     }
