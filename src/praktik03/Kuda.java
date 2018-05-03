4932119defa57696b14b22b37440a069fb1bd11b
Wahyu <adjieb.wahyu@gmail.com>
2018-05-03 19:32:58 +0800

    Buat Attribut Getter and Setter

--- src/praktik03/Kuda.java
+++ src/praktik03/Kuda.java
@@ -8,4 +8,29 @@ public class Kuda {
                 "Warna \t\t: "+warna+"\n"+
                 "Harga \t\t: "+harga);
     }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
 }
