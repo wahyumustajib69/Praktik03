8249bbdb5d9da03b98482abb91e01576daeb48dc
Wahyu <adjieb.wahyu@gmail.com>
2018-05-03 19:25:24 +0800

    Membuat Method Cetakinfo()

--- src/praktik03/Kuda.java
+++ src/praktik03/Kuda.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktik03;
-
-/**
- *
- * @author WAHYU'S PC
- */
 public class Kuda {
+    String jenis,warna;
+    double harga;
     
+    void cetakinfo(){
+        System.out.println("Jenis Kuda \t: "+jenis+"\n"+
+                "Warna \t\t: "+warna+"\n"+
+                "Harga \t\t: "+harga);
+    }
 }
