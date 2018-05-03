11529920b95213288a987de94c737404733f6eb3
Wahyu <adjieb.wahyu@gmail.com>
2018-05-03 19:34:58 +0800

    Menggunakan Method Setter

--- src/praktik03/KudaAksi.java
+++ src/praktik03/KudaAksi.java
@@ -2,10 +2,9 @@ package praktik03;
 public class KudaAksi {
     public static void main(String[] args) {
         Kuda x = new Kuda();
-        x.jenis ="SUMBAWA";
-        x.warna ="Hitam";
-        x.harga=100;
-        
+        x.setJenis("SUMBAWA");
+        x.setWarna("HITAM");
+        x.setHarga(100);
         x.cetakinfo();
     }
 }
